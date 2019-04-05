971b66e91e03628df7a3b04a0c8bf784d8c3f2a0
Qi Zhang <singularitti@outlook.com>
2019-04-05 17:22:37 -0400

    Update 3.py for HW 5

--- HW 5/code/3.py
+++ HW 5/code/3.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -u -i
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 number_of_samples = 100
 s11, s44, s12 = 15, 13.3, -6.3  # s11, s44, s12
+
+
+def reuss_average(s11, s12, s44):
+    return (3 * s11 + 2 * s12 + s44) / 5
 
 
 def Q(theta, phi):
@@ -17,8 +22,13 @@ def s_average(theta, phi):
     return s11 - 2 * (s11 - s12 - s44 / 2) * Q(theta, phi)
 
 
-theta = np.pi * np.random.random(number_of_samples)
-phi = 2 * np.pi * np.random.random(number_of_samples)
-
-print(np.mean([np.mean([s_average(theta[i], phi[i])
-                        for i in range(number_of_samples)]) for i in range(100)]))
+mc_results = []
+number_of_samples = range(100, 1000)
+for n in number_of_samples:
+    theta = np.pi * np.random.random(n)
+    phi = 2 * np.pi * np.random.random(n)
+    mc_results.append(np.mean([s_average(theta[i], phi[i])
+                               for i in range(n)]))
+plt.plot(number_of_samples, mc_results)
+plt.plot(number_of_samples, [reuss_average(
+    s11, s12, s44)] * len(number_of_samples))

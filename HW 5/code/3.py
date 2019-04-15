35a5e96cefc55ec22b301182aab1998d7b3cddb4
Qi Zhang <singularitti@outlook.com>
2019-04-15 00:20:33 -0400

    Fix a physical mistake by not using a sin distribution

--- HW 5/code/3.py
+++ HW 5/code/3.py
@@ -2,13 +2,18 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import rv_continuous
 
-number_of_samples = 100
 s11, s44, s12 = 15, 13.3, -6.3  # s11, s44, s12
 
 
 def reuss_average(s11, s12, s44):
     return (3 * s11 + 2 * s12 + s44) / 5
+
+
+class SinusodialDistributionGenerator(rv_continuous):
+    def _pdf(self, x):
+        return np.sin(x)
 
 
 def Q(theta, phi):
@@ -23,12 +28,20 @@ def s_average(theta, phi):
 
 
 mc_results = []
-number_of_samples = range(100, 1000)
+number_of_samples = range(100, 1000, 10)
+rng = SinusodialDistributionGenerator(a=0, b=np.pi)
 for n in number_of_samples:
-    theta = np.pi * np.random.random(n)
+    theta = rng.rvs(size=n)
     phi = 2 * np.pi * np.random.random(n)
     mc_results.append(np.mean([s_average(theta[i], phi[i])
                                for i in range(n)]))
-plt.plot(number_of_samples, mc_results)
+    # mc_results.append(np.mean([Q(theta[i], phi[i]) for i in range(n)]))
+plt.plot(number_of_samples, mc_results, label="Monte Carlo sampling average")
 plt.plot(number_of_samples, [reuss_average(
-    s11, s12, s44)] * len(number_of_samples))
+    s11, s12, s44)] * len(number_of_samples), label="analytical Reuss average")
+plt.legend(loc="best")
+plt.xlabel("Number of random numbers")
+plt.ylabel("$\\bar{s}_{1111}$")
+plt.title("Monte Carlo sampling VS analytical result")
+# plt.show()
+plt.savefig("mc_reuss.pdf")
